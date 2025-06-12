b6feb778860586b352b43165b23f9cbc3710a0ef
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-06-11 22:09:06 -0300

    termine el program.cs (me olvide de commitear denuevo perdon)

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,1 +1,151 @@
-﻿
+﻿using ToDoTarea;
+
+List<Tarea> tareasPendientes = new List<Tarea>();
+
+List<Tarea> tareasRealizadas = new List<Tarea>();
+
+Random numeroAleatorio = new Random();
+
+int numTareas = 0, opcion; ;
+
+string opcion_texto, numTareas_texto;
+
+bool valido = false;
+do
+{
+
+
+
+    do
+    {
+        Console.WriteLine("-------Menu-------");
+        Console.WriteLine("1-Agregar Tarea/s");
+        Console.WriteLine("2-Ver tareas Pendientes");
+        Console.WriteLine("3-marcar tarea como realizada");
+        Console.WriteLine("4-Ver tareas Realizadas");
+        Console.WriteLine("5-Consultar tarea por descripcion (solo tareas pendientes)");
+        Console.WriteLine("0-salir");
+
+        opcion_texto = Console.ReadLine();
+
+        if (!int.TryParse(opcion_texto, out opcion) || (opcion < 0 || opcion > 5))
+        {
+            Console.WriteLine("Opcion ingresada invalida...");
+        }
+
+    } while (!int.TryParse(opcion_texto, out opcion) || (opcion < 0 || opcion > 5));
+
+    if (opcion == 0)
+    {
+        break;
+    }
+
+    switch (opcion)
+    {
+        case 1:
+            do
+            {
+                Console.WriteLine("Ingrese cuantas tareas desea agregar");
+
+                numTareas_texto = Console.ReadLine();
+
+                if (!int.TryParse(numTareas_texto, out numTareas) || numTareas <= 0)
+                {
+                    Console.WriteLine("numero ingresado invalido...");
+                }
+            } while (!int.TryParse(numTareas_texto, out numTareas) || numTareas <= 0);
+
+            for (int i = 0; i < numTareas; i++)
+            {
+
+                Tarea nueva = new Tarea();
+
+                nueva.tareaID = 1 + i;
+
+                Console.WriteLine("Ingrese una descripcion de la tarea: " + (i + 1));
+
+                nueva.descripcion = Console.ReadLine();
+
+                nueva.validarDuracion();
+
+                tareasPendientes.Add(nueva);
+
+            }
+
+            break;
+
+        case 2:
+
+            Tarea.mostrarTareasPendientes(tareasPendientes);
+
+            break;
+
+        case 4:
+
+            if (tareasRealizadas.Count == 0)
+            {
+                Console.WriteLine("La lista de tareas realizadas esta vacia");
+            }
+            else
+            {
+                foreach (Tarea t in tareasRealizadas)
+                {
+                    Console.WriteLine("-------DATOS TAREAS REALIZADAS-------");
+                    Console.WriteLine("Tarea ID:" + t.tareaID);
+                    Console.WriteLine("Tarea descripcion:" + t.descripcion);
+                    Console.WriteLine("Tarea duracion:" + t.duracion);
+                    Console.WriteLine("-----------------------------------------------");
+                }
+            }
+
+
+            break;
+
+        case 3:
+
+
+            int cantTareas = Tarea.validarRespuesta();
+
+            if (!(cantTareas == 0))
+            {
+                int[] Id_a_mover = Tarea.agregarID(cantTareas, tareasPendientes, numTareas);
+
+                Tarea.moverTareas(Id_a_mover, tareasRealizadas, tareasPendientes, numTareas);
+
+            }
+
+            break;
+
+
+        case 5:
+
+
+            Tarea.buscarTarea(tareasPendientes);
+
+            break;
+    }
+
+    string respuesta;
+
+    do
+    {
+        Console.WriteLine("desea realizar alguna otra operacion? (si/no)");
+
+        respuesta = Console.ReadLine().ToLower();
+
+        if (respuesta != "no" && respuesta != "si")
+        {
+            valido = false;
+            Console.WriteLine("Respuesta invalida...");
+        }
+        else
+        {
+            valido = true;
+        }
+
+    } while (!valido);
+
+
+} while (valido);
+
+Console.WriteLine("Nos vemos rey");
--- ToDo/Tarea.cs
+++ ToDo/Tarea.cs
@@ -3,8 +3,215 @@ namespace ToDoTarea
 
     public class Tarea
     {
-        public int TareaID;
-        public string Descripcion;
-        public int Duracion;
+        public int tareaID;
+        public string descripcion;
+        public int duracion;
+
+        public void validarDuracion()
+        {
+            string duracionTarea;
+
+            do
+            {
+                Console.WriteLine("Ingrese la duracion de la tarea");
+
+                duracionTarea = Console.ReadLine();
+
+                if (!int.TryParse(duracionTarea, out duracion) || !(duracion >= 10) || !(duracion <= 100))
+                {
+                    Console.WriteLine("Escribiste mal la duracion pibe");
+                }
+
+            } while (!int.TryParse(duracionTarea, out duracion) || !(duracion >= 10) || !(duracion <= 100));
+
+
+        }
+
+        public static void mostrarTareasPendientes(List<Tarea> tareasPendientes)
+        {
+            foreach (Tarea t in tareasPendientes)
+            {
+                Console.WriteLine("-------DATOS TAREAS PENDIENTES-------");
+                Console.WriteLine("Tarea ID:" + t.tareaID);
+                Console.WriteLine("Tarea descripcion:" + t.descripcion);
+                Console.WriteLine("Tarea duracion:" + t.duracion);
+                Console.WriteLine("-----------------------------------------------");
+            }
+        }
+
+        public static int validarRespuesta()
+        {
+
+
+
+            string cant_tareas;
+
+            int cantTareas = 0;
+
+
+            do
+            {
+                Console.WriteLine("Ingrese cuantas tareas desea mover");
+
+                cant_tareas = Console.ReadLine();
+
+                if (!int.TryParse(cant_tareas, out cantTareas))
+                {
+                    Console.WriteLine("Vuelva a escribir la cantidad de tareas maestro");
+                }
+                if (cantTareas < 0)
+                {
+                    Console.WriteLine("Bro, no podes mover esa cantidad de tareas, elegi un valor valido dale c:");
+                }
+                if (cantTareas == 0)
+                {
+                    Console.WriteLine("Ok, entonces no movamos ninguna");
+                    
+                }
+
+            } while (!int.TryParse(cant_tareas, out cantTareas) || cantTareas < 0);
+
+
+
+
+            return cantTareas;
+        }
+
+
+        public static int[] agregarID(int cantTareas, List<Tarea> tareasPendientes, int numeroTareas)
+        {
+            int[] IdTareas = new int[cantTareas];
+
+
+
+            for (int i = 0; i < cantTareas; i++)
+            {
+                int id;
+                bool valido;
+                do
+                {
+
+
+                    Console.WriteLine("Ingrese el id de la tarea: " + (i + 1));
+
+                    string id_num = Console.ReadLine();
+
+                    valido = false;
+
+                    if (int.TryParse(id_num, out id))
+                    {
+
+                        foreach (Tarea t in tareasPendientes)
+                        {
+                            if (t.tareaID == id)
+                            {
+                                valido = true;
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine("Se ha cambiado el contenido de la lista");
+
+                    }
+                    if (!valido)
+                    {
+                        Console.WriteLine("No hay ninguna tarea con ese id maestro, proba de nuevo");
+                    }
+
+                } while (!valido);
+
+                IdTareas[i] = id;
+            }
+
+            return IdTareas;
+        }
+
+        public static void moverTareas(int[] idTarea, List<Tarea> tareasRealizadas, List<Tarea> tareasPendientes, int numeroTareas)
+        {
+
+            List<Tarea> tareasParaMover = new List<Tarea>();
+
+            foreach (int id in idTarea)
+            {
+                foreach (Tarea t in tareasPendientes)
+                {
+                    if (id == t.tareaID)
+                    {
+                        tareasParaMover.Add(t);
+                        break;
+                    }
+                }
+            }
+            foreach (Tarea t in tareasParaMover)
+            {
+                tareasRealizadas.Add(t);
+                tareasPendientes.Remove(t);
+
+            }
+
+        }
+
+        public static void buscarTarea(List<Tarea> tareasPendientes)
+        {
+
+            bool valido;
+
+            string respuesta;
+
+            do
+            {
+
+                bool encontrada = false;
+
+                do
+                {
+                    Console.WriteLine("Ingrese la descripcion");
+
+                    string descripcionBuscar = Console.ReadLine();
+
+                    foreach (Tarea t in tareasPendientes)
+                    {
+                        if (descripcionBuscar == t.descripcion)
+                        {
+                            Console.WriteLine("------Tarea pendiente buscada------");
+                            Console.WriteLine("Id de la tarea: " + t.tareaID);
+                            Console.WriteLine("descripcion de la tarea: " + t.descripcion);
+                            Console.WriteLine("duracion de la tarea: " + t.duracion);
+                            encontrada = true;
+                        }
+
+                    }
+
+                    if (!encontrada)
+                    {
+                        Console.WriteLine("no existe tarea con esa descripcion ,proba otra");
+                    }
+                } while (!encontrada);
+
+                do
+                {
+                    Console.WriteLine("Desea buscar alguna otra tarea por descripcion? (si/no)");
+
+                    respuesta = Console.ReadLine().ToLower();
+
+                    if (respuesta != "no" && respuesta != "si")
+                    {
+                        valido = false;
+                        Console.WriteLine("Respuesta invalida...");
+                    }
+                    else
+                    {
+                        valido = true;
+                    }
+
+                } while (!valido);
+
+            } while (respuesta == "si");
+
+            Console.WriteLine("De una maestro 😎");
+        }
+
+
     }
+
 }
